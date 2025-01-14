ab471cf37564248bf56075146697442cd9979ebf
Joaquín Villar <jvillarmart@icloud.com>
2025-01-14 18:51:28 +0100

    finished assignment

---
+++ arithmeti.py
@@ -0,0 +1,8 @@
+print(2+3) # addition
+print(2-3) # substraction
+print(2*3) # multiplication
+print(type(4/2),4/2) # division
+print(type(4//2), 4//2) # integer division
+print(9%5) # modulo
+print(17%5) #again modulo
+print(2**3) # power
